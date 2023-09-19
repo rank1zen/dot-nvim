2b82dcfb08389e56c81628bf91e215f40c647b83
Gordon <gordonchen2014@gmail.com>
2023-09-19 19:54:43 -0400

    chore: telescope

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,3 @@
 vim.keymap.set('n', 'L', '<cmd>bnext<CR>')
 vim.keymap.set('n', 'H', '<cmd>bprevious<CR>')
 vim.keymap.set('n', '<Tab>', '<C-w>w')
-
-vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end)
--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
+  lazy = true,
   opts = {
     defaults = {
       layout_strategy = 'horizontal',
@@ -11,5 +12,17 @@ return {
       scroll_strategy = 'limit',
       prompt_prefix = '>  '
     }
+  },
+  keys = {
+    {
+      '<Leader>ff',
+      function() require('telescope.builtin').find_files() end,
+      desc = 'Telescope find files',
+    },
+    {
+      '<Leader>fF',
+      function() require('telescope.builtin').git_files() end,
+      desc = 'Telescope find Git files',
+    },
   }
 }
