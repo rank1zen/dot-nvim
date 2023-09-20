21b514c97774fcfdfb10137221ecc6837186ce1b
Gordon <gordonchen2014@gmail.com>
2023-09-20 00:55:10 -0400

    add: telescope ui

--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -1,28 +1,39 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  lazy = true,
-  opts = {
-    defaults = {
-      layout_strategy = 'horizontal',
-      layout_config = {
-        height = 0.45,
-        anchor = 'S',
+  {
+    'nvim-telescope/telescope.nvim',
+    lazy = true,
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmds = { 'Telescope' },
+    opts = {
+      defaults = {
+        layout_strategy = 'vertical',
+        layout_config = {
+          height = 0.5,
+          width = 0.5,
+          anchor = 'SW',
+        },
+        prompt_prefix = '>  '
       },
-      scroll_strategy = 'limit',
-      prompt_prefix = '>  '
+    },
+    keys = {
+      {
+        '<Leader>ff',
+        function() require('telescope.builtin').find_files() end,
+        desc = 'Telescope find files',
+      },
+      {
+        '<Leader>gf',
+        function() require('telescope.builtin').git_files() end,
+        desc = 'Telescope find Git files',
+      }
     }
   },
-  keys = {
-    {
-      '<Leader>ff',
-      function() require('telescope.builtin').find_files() end,
-      desc = 'Telescope find files',
-    },
-    {
-      '<Leader>fF',
-      function() require('telescope.builtin').git_files() end,
-      desc = 'Telescope find Git files',
-    },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+    lazy = true,
+    dependencies = 'nvim-telescope/telescope.nvim',
+    config = function()
+      require('telescope').load_extension('ui-select')
+    end
   }
 }
