7e127cb5dcf4f94905b26701d2c9a234736cb773
Gordon <gordonchen2014@gmail.com>
2023-10-17 14:37:01 -0400

    keymap change

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', '<M-h>', '<cmd>bnext<CR>')
-vim.keymap.set('n', '<M-l>', '<cmd>bprevious<CR>')
+vim.keymap.set('n', '<C-h>', '<cmd>bnext<CR>')
+vim.keymap.set('n', '<C-l>', '<cmd>bprevious<CR>')
 
 vim.keymap.set('n', '<Tab>', '<C-w>w')
