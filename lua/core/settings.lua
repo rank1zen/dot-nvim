058d668d8f2779200dcb51cfb1f4210395fcb00b
Gordon <gordonchen2014@gmail.com>
2024-06-01 13:35:12 -0400

    change: vim.o settings

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,13 +1,19 @@
-vim.o.autoindent = true
-vim.o.smartindent = true
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.expandtab = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.softtabstop = -1
+--stylua: ignore start
+vim.g.mapleader = ' '
+-- vim.g.maplocalleader = ' '
 
+vim.o.autoindent     = true
+vim.o.smartindent    = true
+vim.o.number         = true
+vim.o.relativenumber = true
+vim.o.expandtab      = true
+vim.o.tabstop        = 4
+vim.o.shiftwidth     = 4
+vim.o.softtabstop    = -1
+vim.o.cursorline     = true
 -- vim.o.termguicolors = true
+
+-- stylua: ignore end
 
 vim.filetype.add({
   extension = {
