f9fb54d3a08c283c367d4dbdc55c66decab87ee4
Gordon <gordonchen2014@gmail.com>
2024-06-03 18:40:00 -0400

    mod: ftplugins

--- after/ftplugin/templ.lua
+++ after/ftplugin/templ.lua
@@ -1,3 +1,3 @@
 vim.opt_local.expandtab = false
-vim.opt_local.tabstop = 8
-vim.opt_local.shiftwidth = 8
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
--- after/ftplugin/tex.lua
+++ after/ftplugin/tex.lua
@@ -1,2 +1,3 @@
 vim.cmd('lua MiniPairs.map_buf(0, "i", "$", {action = "closeopen", pair = "$$"})')
 vim.opt_local.conceallevel=2
+vim.opt_local.linebreak=true
