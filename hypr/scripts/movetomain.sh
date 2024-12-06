fa069bae1e6322a08d2f7ebd22da1c1adb5f5af3
Gordon <gordonchen2014@gmail.com>
2024-12-06 11:54:23 -0500

    hypr rewrite

--- hypr/scripts/movetomain.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-current_workspace=$(hyprctl activewindow -j | jq '.workspace.name')
-if [ "$current_workspace" = "special:scratch" ]; then
-  address=$(hyprctl activewindow -j | jq '.address')
-  hyprctl dispatch movetoworkspacesilent "main,address:$address"
-  hyprctl dispatch settiled "address:$address"
-fi
